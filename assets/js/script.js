c4dff40eb4e43f0ab3e8d99d7d10e5bbbbab4b2d
coleyrockin <coleyrockin@aol.com>
2022-06-27 14:33:48 -0500

    Added current date and time, moment.js/jquery

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// moment.js stuff
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+//Date and Hour
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100)
+
